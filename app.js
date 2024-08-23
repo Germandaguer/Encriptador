0d273866255ae3b121b348003881339c4fdcb4a6
Germandaguer <150746818+Germandaguer@users.noreply.github.com>
2024-08-22 22:15:15 -0300

    carpeta imagen .inicio js

--- app.js
+++
@@ -1,1 +0,0 @@
-console.log("creando el encriptador y practicando git y github")
---
+++ script.js
@@ -0,0 +1,12 @@
+let textArea = document.querySelector(".text_area");
+let mensajeEncriptado = document.querySelector(".texto_mensaje");
+
+
+function encriptar (encriptarletras) {
+    let llavesDeEncriptar = [["e","enter"] , ["i", "imes"] , ["a","ai"] , ["o","ober"] , ["u", "ufat"]];
+    
+}
+
+
+
+
