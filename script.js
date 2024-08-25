b8cece0c2204371f0a4520ed9d9925145bbae6b1
Germandaguer <150746818+Germandaguer@users.noreply.github.com>
2024-08-24 22:56:15 -0300

    funcion y boton encrip en js

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-let textArea = document.querySelector(".text_area");
-let mensajeEncriptado = document.querySelector(".texto_mensaje");
+const textArea = document.querySelector(".text_area");
+const mensajeEncriptado = document.querySelector(".texto_mensaje");
+
+function botonEncriptar() {
+    let textoEncriptado = encriptar(textArea.value) //creo la variable , llamo la funcion encriptar , y el textArea donde se ingreso el usuario.
+    mensajeEncriptado.value = textoEncriptado;
+    textArea.value = "";
+    mensajeEncriptado.style.backgroundImage = "none";
+    return
+
+}
 
 
-function encriptar (encriptarletras) {
-    let llavesDeEncriptar = [["e","enter"] , ["i", "imes"] , ["a","ai"] , ["o","ober"] , ["u", "ufat"]];
-    
+function encriptar(encriptarletras) {
+    const llavesDeEncriptar = [["e","enter"] , ["i", "imes"] , ["a","ai"] , ["o","ober"] , ["u", "ufat"]];
+
+    for(i=0; i < llavesDeEncriptar.length; i++){ // recorro todo el array llavesDeEncriptar
+
+        if (encriptarletras.includes(llavesDeEncriptar [i][0])){ // si, con el metodo include (solo strings) verfico si la subcadena contiene el valor especifica "a", "e".etc en una cadena mas larga.
+
+            encriptarletras = encriptarletras.replaceAll(llavesDeEncriptar [i][0] , llavesDeEncriptar [i][1]); // con replaceall remplazo los valores del array, primero los valores de las llaves iniciales [o] por los valores [1]
+
+        }
+        
+
+    } 
+    return encriptarletras
 }
 
 
