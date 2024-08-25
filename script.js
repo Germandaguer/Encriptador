e5d2e656caa2bcf2c91c5f39db80054fabc39558
Germandaguer <150746818+Germandaguer@users.noreply.github.com>
2024-08-25 15:53:32 -0300

    terminado con boton de copiar

--- script.js
+++ script.js
@@ -10,11 +10,31 @@ function botonEncriptar() {
 
 }
 
+function botonDesencriptar() {
+    let textoDesencriptado = desencriptar(textArea.value);
+    mensajeEncriptado.value = textoDesencriptado;
+    textArea.value = "";
+    mensajeEncriptado.style.backgroundImage = "none";
+    return
+    
+}
+
+function botonCopiar() {
+    // Obtén el elemento con la clase 'texto_mensaje'
+    let copiarTexto = document.querySelector(".texto_mensaje");
+
+    // Selecciona el texto del elemento
+    copiarTexto.select();
+    copiarTexto.setSelectionRange(0, 99999); // Para dispositivos móviles 
+    // Copia el texto al portapapeles
+    document.execCommand('copy');
+    }
+
 
 function encriptar(encriptarletras) {
     const llavesDeEncriptar = [["e","enter"] , ["i", "imes"] , ["a","ai"] , ["o","ober"] , ["u", "ufat"]];
 
-    for(i=0; i < llavesDeEncriptar.length; i++){ // recorro todo el array llavesDeEncriptar
+    for(let i = 0; i < llavesDeEncriptar.length; i++){ // recorro todo el array llavesDeEncriptar
 
         if (encriptarletras.includes(llavesDeEncriptar [i][0])){ // si, con el metodo include (solo strings) verfico si la subcadena contiene el valor especifica "a", "e".etc en una cadena mas larga.
 
@@ -25,6 +45,21 @@ function encriptar(encriptarletras) {
 
     } 
     return encriptarletras
+}
+
+function desencriptar (desencriptarLetras) {
+    const llavesDeEncriptar = [["e","enter"] , ["i", "imes"] , ["a","ai"] , ["o","ober"] , ["u", "ufat"]];
+
+    for (let i = 0; i < llavesDeEncriptar.length; i++) {
+
+        if (desencriptarLetras.includes(llavesDeEncriptar[i][1])) {
+
+            desencriptarLetras = desencriptarLetras.replaceAll(llavesDeEncriptar [i][1], llavesDeEncriptar [i][0]);
+        
+        }
+        
+    }
+    return desencriptarLetras
 }
 
 
